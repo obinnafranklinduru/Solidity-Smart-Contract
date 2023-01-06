c443d230d7bdce5e7541a9f3daf1c67ec045a5fa
obinnafranklinduru <obinnafranklinduru.com>
2023-01-06 18:28:02 +0100

    initial commit

---
+++ MultSIg-Wallet/MultSig_Wallet.sol
@@ -0,0 +1,308 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract MultiSigWallet{
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        string data
+    );
+    event SetNumConfirmationsRequired(uint indexed numConfirmationsRequired);
+    event Pause();
+    event Unpause();
+    event Recovery(address indexed oldAccount, address indexed newAccount);
+
+    mapping(address => bool) public isOwner;
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
+
+    address[] public owners;
+    Transaction[] public transactions;
+
+    uint256 public numConfirmationsRequired;
+    bool paused;
+
+    struct Transaction {
+        address payable to;
+        uint value;
+        string data;
+        bool executed;
+        uint256 numConfirmations;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender] = true, "Avaiable to Owners");
+        _;
+    }
+    modifier txExists(uint256 _txIndex) {
+        require(_txIndex < transactions.length, "Tx does not exist");
+        _;
+    }
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "Tx already comfirmed");
+        _;
+    }
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "Tx already executed");
+        _;
+    }
+    modifier whenNotPaused() {
+        require(!paused, "Wallet is paused");
+        _;
+    }
+
+    /**
+     * constructor function is used to create a new instance of the contract and set the initial owners and number of required confirmations.
+     * 
+     * @param _owners: An array of addresses representing the initial owners of the wallet
+     * @param _numConfirmationsRequired: The number of required confirmations for a transaction to be executed
+     */
+    constructor(address[] memory _owners, uint256 _numConfirmationsRequired){
+        require(_owners.length > 0, "Invalid number of member");
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired < _owners.length,
+            "invalid number of required confirmations"
+        );
+        
+        for(uint256 i; i < _owners.length; i++){
+            address owner = _owners[i];
+            require(owner != address(0), "Invalid Address");
+            require(!isOwner[owner], "owner is not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    /**
+     * addOwner: This function allows an existing owner to add a new owner to the wallet.
+     * 
+     * @param _account: The address of the new owner
+     * 
+     * Requirements
+     *  The caller must be an existing owner of the wallet
+     *  The new owner's address must not already be an owner of the wallet
+     */
+    function addOwner(address _account) external onlyOwner {
+        require(!isOwner[_account], "Address not unique");
+        require(_account != address(0), "Invalid Address");
+        isOwner[_account] = true;
+        owners.push(_account);
+        numConfirmationsRequired++;
+    }
+
+    /**
+     * removeOwner: This function allows an existing owner to remove an existing owner from the wallet.
+     * 
+     * @param _account: The address of the owner to remove
+     * 
+     * Requirements
+     *  The caller must be an existing owner of the wallet
+     *  The owner to remove must be an existing owner of the wallet
+     *  The wallet must have more than one owner
+     */
+    function removeOwner(address _account) external onlyOwner {
+        bool findMember;
+        uint256 indexedMember;
+        require(owners.length > 1, "Cannot remove the last owner of the wallet");
+        for(uint256 i = 0; i < owners.length; i++){
+            if(_account == owners[i]){
+                findMember = true;
+                indexedMember = i;
+                break;
+            }
+        }
+        require(findMember, "Not an owner of the wallet");
+        isOwner[owners[indexedMember]] = false;
+        owners[indexedMember] = owners[owners.length - 1];
+        owners.pop();
+        numConfirmationsRequired--;
+    }
+
+    /**
+     * submitTransaction: This function allows an owner to submit a transaction to be executed by the wallet.
+     * 
+     * @param _to: The address of the recipient of the transaction
+     * @param _data: Additional data to include in the transaction
+     * 
+     * Requirements
+     *  The caller must be an owner of the wallet
+     *  The wallet must not be paused
+     */
+    function submitTransaction(address payable _to, string memory _data) 
+    payable 
+    public
+    whenNotPaused 
+    onlyOwner 
+    {
+        uint txIndex = transactions.length;
+        
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: msg.value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, msg.value, _data);
+    }
+
+    /**
+     * confirmTransaction: This function allows an owner to confirm a transaction.
+     * @param _txIndex: The index of the transaction to confirm
+     * 
+     * Requirements
+     *  The caller must be an owner of the wallet
+     *  The transaction must not already have been confirmed by the caller
+     *  The transaction must exist
+     *  The wallet must not be paused
+     */
+    function confirmTransaction(uint _txIndex) 
+    public 
+    onlyOwner 
+    txExists(_txIndex) 
+    notExecuted(_txIndex) 
+    notConfirmed(_txIndex) 
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+
+        emit ConfirmTransaction(msg.sender, _txIndex);
+    }
+
+    function executeTransaction(uint _txIndex) 
+    public 
+    onlyOwner 
+    txExists(_txIndex)
+    whenNotPaused 
+    notExecuted(_txIndex) 
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "cannot execute tx"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}("");
+        require(success, "tx failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
+
+    function revokeConfirmation(uint _txIndex) 
+    public 
+    onlyOwner 
+    txExists(_txIndex) 
+    notExecuted(_txIndex) 
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
+
+        transaction.numConfirmations -= 1;
+        isConfirmed[_txIndex][msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
+
+    function pause() external onlyOwner {
+        require(!paused, "Wallet is already paused");
+        paused = true;
+        emit Pause();
+    }
+
+    function unpause() external onlyOwner {
+        require(paused, "Wallet is not paused");
+        paused = false;
+        emit Unpause();
+    }
+
+    function emergencyWithdrawal(address payable _to) external {
+        require(isOwner[msg.sender], "Only an owner can perform an emergency withdrawal");
+        require(address(this).balance == 0, "Cannot perform emergency withdrawal unless the wallet is empty");
+
+        uint256 deadline = block.timestamp + 24 hours;
+        require(block.timestamp < deadline, "Emergency withdrawal period has expired");
+        _to.transfer(address(this).balance);
+    }
+
+    function setNumConfirmationsRequired(uint256 _numConfirmationsRequired) external onlyOwner {
+        require(_numConfirmationsRequired > 0 && _numConfirmationsRequired <= owners.length, "Invalid number of required confirmations");
+        numConfirmationsRequired = _numConfirmationsRequired;
+
+        emit SetNumConfirmationsRequired(_numConfirmationsRequired);
+    }
+
+    function changeOwner(address _oldOwner, address _newOwner) external onlyOwner {
+        require(isOwner[_oldOwner], "Old owner is not an owner of the wallet");
+        require(!isOwner[_newOwner], "New owner is already an owner of the wallet");
+        require(_newOwner != address(0), "Invalid new owner address");
+
+        isOwner[_oldOwner] = false;
+        isOwner[_newOwner] = true;
+
+        // Update the owners array to reflect the change in ownership
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (owners[i] == _oldOwner) {
+                owners[i] = _newOwner;
+                break;
+            }
+        }
+
+        emit Recovery(_oldOwner, _newOwner);
+    }
+
+    function totalSupply() external view returns (uint256){
+        return (address(this)).balance;
+    }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
+
+    function getTransaction(
+        uint _txIndex
+    )
+        public
+        view
+        returns (
+            address to,
+            uint value,
+            string memory data,
+            bool executed,
+            uint256 numConfirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
+    }
+
+    receive() payable external {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+}
---
+++ MultSIg-Wallet/README.md
@@ -0,0 +1,60 @@
+# MultiSigWallet
+This is a Solidity implementation of a multi-signature wallet contract. It allows a group of owners to jointly manage a shared wallet and requires a certain number of confirmations from owners before a transaction can be executed.
+
+### Features
+- Add and remove wallet owners
+- Deposit funds into the wallet
+- Submit transactions to be confirmed and executed by the owners
+- Confirm and revoke transaction confirmations
+- Set the required number of confirmations for transactions
+- Pause and unpause the wallet to prevent transactions from being submitted or executed
+- Recover the wallet in the event that it becomes stranded or otherwise unable to function
+
+### Events
+- Deposit: Emitted when funds are deposited into the wallet.
+- ConfirmTransaction: Emitted when an owner confirms a transaction.
+- RevokeConfirmation: Emitted when an owner revokes their confirmation of a transaction.
+- ExecuteTransaction: Emitted when a transaction is executed by the wallet.
+- SubmitTransaction: Emitted when a transaction is submitted to the wallet.
+- SetNumConfirmationsRequired: Emitted when the required number of confirmations is changed.
+- Pause: Emitted when the wallet is paused.
+- Unpause: Emitted when the wallet is unpaused.
+- Recovery: Emitted when an owner recovers their funds to a new address.
+
+### Usage
+To use the contract, call the `constructor` function with an array of addresses for the initial wallet owners and an integer for the required number of confirmations.
+`constructor(address[] memory _owners, uint256 _numConfirmationsRequired)`
+
+### Functions
+#### constructor(address[] memory _owners, uint256 _numConfirmationsRequired)
+The constructor is called when the contract is deployed and sets the initial list of owners and the number of confirmations required for a transaction to be executed.
+
+#### addOwner(address _account)
+This function allows an existing owner to add a new owner to the wallet.
+
+#### removeOwner(address _account)
+This function allows an existing owner to remove another owner from the wallet. It is not possible to remove the last owner.
+
+#### submitTransaction(address payable _to, string memory _data)
+This function allows an owner to submit a new transaction to the wallet. The transaction includes the recipient address, the value (in wei) to be sent, and an optional data field.
+
+#### confirmTransaction(uint256 _txIndex)
+This function allows an owner to confirm a transaction. Once enough confirmations have been received, the transaction can be executed.
+
+#### revokeConfirmation(uint256 _txIndex)
+This function allows an owner to revoke their confirmation for a transaction. If a transaction does not have enough confirmations after one is revoked, it cannot be executed.
+
+#### executeTransaction(uint256 _txIndex)
+This function allows an owner to execute a confirmed transaction.
+
+#### setNumConfirmationsRequired(uint256 _newNumConfirmationsRequired)
+This function allows an owner to change the number of confirmations required for a transaction to be executed.
+
+#### pause()
+This function allows an owner to pause the wallet, which prevents any transactions from being submitted or executed.
+
+#### unpause()
+This function allows an owner to unpause the wallet, allowing transactions to be submitted and executed again.
+
+#### recovery(address _oldAccount, address _newAccount)
+This function allows an owner to change the owner of the contract to a new Ethereum account in the event that the old owner's account is compromised or lost.
